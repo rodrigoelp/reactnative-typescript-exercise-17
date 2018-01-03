dc11c7d77a66ce263a6408586b145b775e4310e3
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-03 11:56:15 +1100

    Changed the react app to display a better message.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,45 @@
 import * as React from "react";
-import { AppRegistry, View, Text } from "react-native";
+import { AppRegistry, StyleSheet, View, Text } from "react-native";
+
+const styles = StyleSheet.create({
+    textStyle: {
+        color: "#131313",
+        fontWeight: "100",
+        textAlign: "center",
+        textAlignVertical: "center",
+        paddingHorizontal: 16
+    },
+    mainGreeting: {
+        fontSize: 30,
+    },
+    followUpGreeting: {
+        fontSize: 24
+    }
+});
 
 class AppShell extends React.PureComponent {
     public render() {
-        const greetings = "Greetings from the other side!\nThis code is running in react native from a swift app!";
+        const greetings = "Hello\n\nIt's me...\n\nHello from the other side!\n\nThis code is running in react native from a swift app!";
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingTop: 44 }}>
-                <Text style={{ fontSize: 30, textAlign: "center", textAlignVertical: "center" }}>
-                    {greetings}
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingTop: 44, backgroundColor: "#ccccff" }}>
+                <Text style={styles.textStyle}>
+                    <Text style={styles.mainGreeting}>
+                        Hello...{"\n\n"}
+                    </Text>
+                    <Text style={styles.followUpGreeting}>
+                        It's me,{"\n\n"}
+
+                        I was wondering if Swift and I would like to meet.{"\n\n"}
+
+                        To grow older...{"\n\n"}
+
+                        And mixed..{"\n\n"}
+
+                        To build some awesome products without people being mean...{"\n\n"}
+                    </Text>
+                    <Text style={styles.mainGreeting}>
+                        Hello from the otherside!!!{"\n\n"}
+                    </Text>
                 </Text>
             </View>
         );
